8861c64aab2e171bb0a05877716134ae8745183d
davidpensjo <david.pensjo@gmail.com>
2023-12-12 09:35:22 +0100

    small change to app.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <h4
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </h4>
       </header>
     </div>
   );
