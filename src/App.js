65430cd0b5e731e252943b251b3b660ca5ae15f2
davidpensjo <david.pensjo@gmail.com>
2023-12-12 10:30:02 +0100

    fml

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>Kony2012</h3>
       </header>
     </div>
   );
